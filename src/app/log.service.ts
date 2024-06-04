02f4c243f6cebfc98faa06cc6987d3c4fd5a9cb8
citlaly <utti212006@utvco.edu.mx>
2024-06-04 11:59:01 -0600

    servicio

---
+++ src/app/log.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LogService {
+
+  constructor(
+    logMessage{
+      (console.log(Hola Mundo);)
+    }
+  ) { }
+
+
+
+}
